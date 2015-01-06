ad174c9e899124212d0b6c96250c24c01c65a8a9
hthngoc <hthngoc@tma.com.vn>
2015-01-06 14:34:20 +0700

    Multi thread

--- MarineBoxAnalysis/Program.cs
+++ MarineBoxAnalysis/Program.cs
@@ -1,162 +1,143 @@
-﻿using System.Net.NetworkInformation;
+﻿using System;
 using System.Threading;
-using System.Diagnostics;
+using System.Net.NetworkInformation;
 using System.Collections.Generic;
-using System;
-using Microsoft.Win32;
 using System.IO;
 
-namespace PingTest
+public class MonitorSample
 {
-    public class PingNetwork
+
+    public static string IPNetwork = ""; 
+    public static string baseIP = "";
+    public static List<string> activeIP = new List<string>();
+    
+    public static void Main(String[] args)
     {
-        public static List<Ping> pingers = new List<Ping>();
-        public static int instances = 0;
-
-        public static object @lock = new object();
-
-        public static int result = 0;
-        public static int timeOut = 250;
-
-        public static int ttl = 5;
-        public static string totallines = "";
-
-        public static void Ping_completed(object s, PingCompletedEventArgs e)
+        int result = 0;   // Result initialized to say there is no error
+        string fn = "ResultOfPing.txt";
+        File.Delete(fn);
+        
+        // Threads producer and consumer have been created, 
+        // but not started at this point.
+        foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
         {
-            lock (@lock)
+            Console.WriteLine();
+            Console.WriteLine(ni.Name);
+            if (ni.OperationalStatus != OperationalStatus.Up) continue;
+            String IPv4 = "";
+            Console.WriteLine("Operational? {0}", ni.OperationalStatus == OperationalStatus.Up);
+            Console.WriteLine("MAC: {0}", ni.GetPhysicalAddress());
+            Console.WriteLine("Gateways:");
+            foreach (GatewayIPAddressInformation gipi in ni.GetIPProperties().GatewayAddresses)
             {
-                instances -= 1;
+                IPNetwork += gipi.Address + Environment.NewLine;
+                Console.WriteLine("\t{0}", gipi.Address);
+            }
+            IPNetwork += Environment.NewLine +  "IP Addresses: ";
+            Console.WriteLine("IP Addresses:");
+            foreach (UnicastIPAddressInformation uipi in ni.GetIPProperties().UnicastAddresses)
+            {
+                if (uipi.Address.AddressFamily.ToString() != "InterNetworkV6")
+                {
+                    if (uipi.Address.ToString() == "127.0.0.1") continue;
+                    IPv4 = uipi.Address.ToString();
+                    IPNetwork += uipi.Address.ToString() + " Subnet Mask: " + uipi.IPv4Mask.ToString() + Environment.NewLine;
+                    Console.WriteLine("\t{0} / {1}", uipi.Address, uipi.IPv4Mask);
+                }
             }
 
-            if (e.Reply.Status == IPStatus.Success)
+            if (IPv4 == "" || IPv4 == "127.0.0.1") continue;
+
+            //ping entire network of NICs
+
+            baseIP = IPv4.Substring(0, IPv4.LastIndexOf(".") + 1);
+
+            Thread producer = new Thread(new ThreadStart(MonitorSample.TryPinging));
+            Thread consumer = new Thread(new ThreadStart(MonitorSample.TryPinging2));
+            Thread thread3 = new Thread(new ThreadStart(MonitorSample.TryPinging3));
+            try
             {
-                Console.WriteLine(string.Concat("Active IP: ", e.Reply.Address.ToString()));
-                totallines += string.Concat("Active IP: ", e.Reply.Address.ToString() + Environment.NewLine);
-                result += 1;
+                producer.Start();
+                consumer.Start();
+                thread3.Start();
+                producer.Join();   // Join both threads with no timeout
+                // Run both until done.
+                consumer.Join();
+                thread3.Join();
+                // threads producer and consumer have finished at this point.
             }
-            else
+            catch (ThreadStateException e)
             {
-                //Console.WriteLine(String.Concat("Non-active IP: ", e.Reply.Address.ToString()))
+                Console.WriteLine(e);  // Display text of exception
+                result = 1;            // Result says there was an error
             }
+            catch (ThreadInterruptedException e)
+            {
+                Console.WriteLine(e);  // This exception means that the thread
+                // was interrupted during a Wait
+                result = 1;            // Result says there was an error
+            }
+            Console.WriteLine("Found " + activeIP.Count.ToString());
+           
         }
+        // Even though Main returns void, this provides a return code to 
+        // the parent process.
+        Environment.ExitCode = result;
 
 
-        public static void CreatePingers(int cnt)
-        {
-            for (int i = 1; i <= cnt; i++)
-            {
-                Ping p = new Ping();
-                p.PingCompleted += Ping_completed;
-                pingers.Add(p);
-            }
-        }
-
-        public static void DestroyPingers()
-        {
-            foreach (Ping p in pingers)
-            {
-                p.PingCompleted -= Ping_completed;
-                p.Dispose();
-            }
-
-            pingers.Clear();
-
-        }
+        File.AppendAllText(fn, IPNetwork);
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("==================     FINISH    ===========================");
+        Console.WriteLine("Please send the file \"ResultOfPing.txt\" for Wallem support");
+        Console.WriteLine("Please Press Any Key to quit");
+        Console.ReadLine();
     }
 
-    class Program
+    public static void TryPinging()
     {
-
-        public static void Main()
+        for (int i = 1; i < 75; i++)
         {
-            string fn = "ResultOfPing.txt";
-            File.Delete(fn);
-            String IPNetwork = "";
-            List<string> activeIP = new List<string>();
-
-            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
-            {
-                Console.WriteLine(ni.Name);
-                if (ni.OperationalStatus != OperationalStatus.Up) continue;
-
-                IPNetwork += "MAC: " + ni.GetPhysicalAddress() + Environment.NewLine;
-                IPNetwork += "Gateways:" + Environment.NewLine;
-
-                Console.WriteLine("Operational? {0}", ni.OperationalStatus == OperationalStatus.Up);
-                Console.WriteLine("MAC: {0}", ni.GetPhysicalAddress());
-                Console.WriteLine("Gateways:");
-                foreach (GatewayIPAddressInformation gipi in ni.GetIPProperties().GatewayAddresses)
-                {
-                    IPNetwork += gipi.Address + Environment.NewLine;
-                    Console.WriteLine("\t{0}", gipi.Address);
-                }
-                IPNetwork += "IP Addresses: ";
-                Console.WriteLine("IP Addresses:");
-                String IPv4 = "";
-                foreach (UnicastIPAddressInformation uipi in ni.GetIPProperties().UnicastAddresses)
-                {
-                    if (uipi.Address.AddressFamily.ToString() != "InterNetworkV6")
-                    {
-                        if (uipi.Address.ToString() == "127.0.0.1") continue;
-                        IPv4 = uipi.Address.ToString();
-                        IPNetwork += uipi.Address.ToString() + " Subnet Mask: " + uipi.IPv4Mask.ToString() + Environment.NewLine;
-                        Console.WriteLine("\t{0} / {1}", uipi.Address, uipi.IPv4Mask);
-                    }
-                }
-                
-                if (IPv4 == "" || IPv4 == "127.0.0.1") continue;
-
-                //ping entire network of NICs
-                string baseIP = IPv4.Substring(0, IPv4.LastIndexOf(".") + 1);
-
-                Console.WriteLine("Pinging 255 destinations of D-class in {0}*", baseIP);
-                PingNetwork.totallines = "";
-
-                //number of IP in subnet
-                PingNetwork.CreatePingers(255);
-
-                PingOptions po = new PingOptions(PingNetwork.ttl, true);
-                System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
-                byte[] data = enc.GetBytes("abababababababababababababababab");
-
-                //SpinWait wait = new SpinWait();
-                //Number of start IP address
-                int cnt = 1;
-                int index = 1;
-                // hthngoc - remove async running due to missing Active IP - to check RQA server.
-                // must run slowly in sequence.
-                foreach (Ping p in PingNetwork.pingers)
-                {
-                    PingReply reply = p.Send(string.Concat(baseIP, cnt.ToString()), PingNetwork.timeOut);
-                    if (reply.Status == IPStatus.Success)
-                    {
-                        Console.WriteLine(index.ToString() + string.Concat(". Active IP: ", reply.Address.ToString()));
-                        activeIP.Add(reply.Address.ToString());
-                        IPNetwork += Environment.NewLine + index.ToString() + ". " + reply.Address.ToString();
-                        index += 1;
-                    }
-                    else {
-                        IPNetwork += Environment.NewLine + "Skipped: " + string.Concat(baseIP, cnt.ToString());
-                    }
-                    cnt += 1;
-                }
-                PingNetwork.DestroyPingers();
-                Console.WriteLine("Found {0} active IP-addresses.", activeIP.Count);
-                IPNetwork += Environment.NewLine + "Found: " + activeIP.Count + Environment.NewLine;
-            }
-            //write to log file            
-            File.AppendAllText(fn, IPNetwork);
-            Console.WriteLine("=========");
-            Console.WriteLine("=========");
-            Console.WriteLine("=========");
-            Console.WriteLine("=========");
-            Console.WriteLine("=========");
-            Console.WriteLine("Done, please Press Any Key to quit. \n\rPlease get the file \"ResultOfPing.txt\" for Wallem support");
-            Console.WriteLine("=========");
-            Console.WriteLine("=========");
-            Console.WriteLine("=========");
-            Console.WriteLine("=========");
-            Console.ReadKey();
+            string ip = baseIP +i.ToString();
+            TryPing0(i,ip);
         }
     }
+    public static void TryPinging2()
+    {
+        for (int i = 76; i < 154; i++)
+        {
+            string ip = baseIP + i.ToString();
+            TryPing0(i, ip);
+        }
+    }
+    public static void TryPinging3()
+    {
+        for (int i = 155; i < 256; i++)
+        {
+            string ip = baseIP + i.ToString();
+            TryPing0(i, ip);
+        }
+    }
+    public static void TryPing0(int index, string destinationIP)
+    {
+        Ping p = new Ping();
+        PingReply reply = p.Send(destinationIP,250);
+        if (reply.Status == IPStatus.Success)
+        {
+            Console.WriteLine(string.Concat("Active IP: ", reply.Address.ToString()));
+            object sync = new Object();
+            lock (sync)
+            {
+                activeIP.Add(reply.Address.ToString());
+                IPNetwork += Environment.NewLine + reply.Address.ToString();
+            }
+        }
+        else
+        {
+            IPNetwork += Environment.NewLine + "Skipped: " + destinationIP;
+        }    
+    }
+
 }
